3789f491e8008216acf02996fe410a624e78567c
root <root@DESKTOP-EAOO8PJ.localdomain>
2024-12-31 10:18:57 +1100

    Chapter 1

---
+++ Chapter1/hello_cargo/target/debug/deps/hello_cargo-e7e2b3e25de0bba2.d
@@ -0,0 +1,5 @@
+/mnt/c/Users/Alex/Documents/RUST/hello_cargo/target/debug/deps/hello_cargo-e7e2b3e25de0bba2: src/main.rs
+
+/mnt/c/Users/Alex/Documents/RUST/hello_cargo/target/debug/deps/hello_cargo-e7e2b3e25de0bba2.d: src/main.rs
+
+src/main.rs:
---
+++ Chapter1/hello_cargo/target/debug/hello_cargo.d
@@ -0,0 +1,1 @@
+/mnt/c/Users/Alex/Documents/RUST/hello_cargo/target/debug/hello_cargo: /mnt/c/Users/Alex/Documents/RUST/hello_cargo/src/main.rs
